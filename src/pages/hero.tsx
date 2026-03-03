1827c888711274a404ddd6b05892224bb4c16043
Abdulganiy Ibrahim <abdulgaiyibrahim095@gmail.com>
2026-03-03 20:06:25 +0100

    adjust the home hero spacing and added functionality to the buttons

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,7 +1,16 @@
 import './hero.css'
 import heroImage from '../assets/hero-image.png'
+import {Link} from 'react-router'
 
 function Hero() {
+
+  const handleClick = () => {
+    const phoneNumber = "2347062639160";
+    const message = "Hello Laitech, I want to build a website.";
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
+  };
+
   return (
     <div className="hero">
       <img 
@@ -12,8 +21,11 @@ function Hero() {
         <h1>Your partner in technology solutions</h1>
         <p>Professional Tech solutions for your business. We build website, We sell Phone, and We fix Software Issue in phone</p>
         <div className='hero-buttons'>
-          <button className="learn-more-btn">Learn More</button>
-          <button className='contact-btn'>Contact Us</button>
+          <Link to="/about">
+            <button className="learn-more-btn">Learn More</button>
+          </Link>
+          
+          <button onClick={handleClick} className='contact-btn'>Contact Us</button>
         </div>
         
       </div>
